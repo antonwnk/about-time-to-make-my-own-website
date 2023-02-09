c419821dc541988869109115f38ead96f134d544
Anton Waniek <antonwnk@users.noreply.github.com>
2023-02-10 01:52:14 +0200

    Initl commit,wanted't start strong but 403 on deps

---
+++ todo.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+# follow https://rustwasm.github.io/book/game-of-life/hello-world.html
+
+cargo install cargo-generate
+
+cargo generate --git https://github.com/rustwasm/wasm-pack-template  # << wasm-app
+
+# publish as github page
+
+:'
+
+
+:wq pwp
+
+'
